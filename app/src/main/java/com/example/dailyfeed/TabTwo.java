1171a38d8ec3250f5514782edda66c9aad20374c
Devang Thakkar <devangjthakkar@gmail.com>
2019-10-25 21:09:47 +0530

    minor fixes

--- app/src/main/java/com/example/dailyfeed/TabTwo.java
+++ app/src/main/java/com/example/dailyfeed/TabTwo.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -23,11 +24,12 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class TabTwo extends Fragment {
+public class TabTwo extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     RecyclerView recyclerView;
     ArrayList<FavouriteItems> favData;
     FavouriteAdapter favouriteAdapter;
+    SwipeRefreshLayout swipeRefreshLayout;
 
 
     @Nullable
@@ -35,16 +37,34 @@ public class TabTwo extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_2, container, false);
         recyclerView = v.findViewById(R.id.recycler_fragment_two);
+        swipeRefreshLayout = v.findViewById(R.id.swipe_tab_two);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        DailyFeedModel.open();
-        favData = DailyFeedModel.showFavourites();
-        DailyFeedModel.close();
-        favouriteAdapter = new FavouriteAdapter(favData, getActivity());
-        recyclerView.setAdapter(favouriteAdapter);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.swipe1, R.color.swipe2, R.color.swipe3);
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                DailyFeedModel.open();
+                favData = DailyFeedModel.showFavourites();
+                DailyFeedModel.close();
+                favouriteAdapter = new FavouriteAdapter(favData, getActivity());
+                recyclerView.setAdapter(favouriteAdapter);
+            }
+        });
 
       return v;
     }
 
 
+    @Override
+    public void onRefresh() {
+        swipeRefreshLayout.setRefreshing(true);
+        DailyFeedModel.open();
+        favData = DailyFeedModel.showFavourites();
+        DailyFeedModel.close();
+        favouriteAdapter = new FavouriteAdapter(favData, getActivity());
+        recyclerView.setAdapter(favouriteAdapter);
+        swipeRefreshLayout.setRefreshing(false);
+    }
 }
