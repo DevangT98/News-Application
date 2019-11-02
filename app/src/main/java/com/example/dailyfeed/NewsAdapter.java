78e202fd217547788c7cb18be0847a0e7fb42e43
Devang Thakkar <devangjthakkar@gmail.com>
2019-11-02 18:26:40 +0530

    Added text to speech for listening to news.

--- app/src/main/java/com/example/dailyfeed/NewsAdapter.java
+++ app/src/main/java/com/example/dailyfeed/NewsAdapter.java
@@ -1,7 +1,11 @@
 package com.example.dailyfeed;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +26,9 @@ import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
@@ -31,7 +37,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     ArrayList<FavouriteItems> favData;
     private Context context;
     int checked = 0;
-
+    TextToSpeech tts;
 
     public NewsAdapter(List<ListItems> listItems, Context context) {
         this.listItems = listItems;
@@ -59,72 +65,124 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
                 .into(holder.newsImage);
 
 
-        holder.explore.setOnClickListener(new View.OnClickListener() {
+        holder.speaker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), ExploreNews.class);
-                i.putExtra("newsUrl", listItems.get(position).getmDetailURL());
-                v.getContext().startActivity(i);
+                tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            tts.setLanguage(Locale.ENGLISH);
+                            playNextChunk(listItem.getmHeading());
+                        }
+                    }
+                });
+
             }
+
+            private void playNextChunk(String text) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ttsGreater21(text);
+                } else {
+                    ttsUnder20(text);
+                }
+            }
+
+            @SuppressWarnings("deprecation")
+            private void ttsUnder20(String text) {
+                HashMap<String, String> map = new HashMap<>();
+                map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+            }
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            private void ttsGreater21(String text) {
+                String utteranceId = this.hashCode() + "";
+                Bundle params = new Bundle();
+                params.putString(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "");
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, params, utteranceId);
+            }
+
         });
 
-        holder.share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, listItems.get(position).getmDetailURL() + "\n\nDownload DailyFeed for more exciting and instant news!");
-                v.getContext().startActivity(i);
-            }
-        });
-        final ListItems like = listItems.get(position);
-        final String url = like.getmDetailURL();
-        final String desc = like.getmDescription();
-        final String title = like.getmHeading();
-        final String imgUrl = like.getmImageURL();
-        final String date = like.getmPublishedAt();
+        holder.explore.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        Intent i = new Intent(v.getContext(), ExploreNews.class);
+        i.putExtra("newsUrl", listItems.get(position).getmDetailURL());
+        v.getContext().startActivity(i);
+    }
+    });
+
+        holder.share.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_TEXT, listItems.get(position).getmDetailURL() + "\n\nDownload DailyFeed for more exciting and instant news!");
+        v.getContext().startActivity(i);
+    }
+    });
+    final ListItems like = listItems.get(position);
+    final String url = like.getmDetailURL();
+    final String desc = like.getmDescription();
+    final String title = like.getmHeading();
+    final String imgUrl = like.getmImageURL();
+    final String date = like.getmPublishedAt();
 
 
         DailyFeedModel.getInstance(context);
         DailyFeedModel.open();
-        final ArrayList<String> alid = DailyFeedModel.getAllIds();
-        if (alid.contains(title)) {
-            holder.heart.setChecked(true);/*
-            holder.heart.setBackgroundResource(R.drawable.like_red);*/
-        } else {
-            holder.heart.setChecked(false);
-            holder.heart.setBackgroundResource(R.drawable.unlike_borderless);
+    final ArrayList<String> alid = DailyFeedModel.getAllIds();
+        if(alid.contains(title))
 
-        }
+    {
+        holder.heart.setChecked(true);/*
+            holder.heart.setBackgroundResource(R.drawable.like_red);*/
+    } else
+
+    {
+        holder.heart.setChecked(false);
+        holder.heart.setBackgroundResource(R.drawable.unlike_borderless);
+
+    }
 
 
         DailyFeedModel.close();
 
-        holder.heart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        holder.heart.setOnClickListener(new View.OnClickListener()
 
-                if (holder.heart.isChecked()) {
-                    holder.heart.setChecked(true);
+    {
+        @Override
+        public void onClick (View v){
+
+        if (holder.heart.isChecked()) {
+            holder.heart.setChecked(true);
 
 
-                    checked = 1;
+            checked = 1;
+            DailyFeedModel.open();
+            DailyFeedModel.insert(title, desc, imgUrl, url, date, checked);
+            DailyFeedModel.close();
+            Snackbar.make(v, "Added to favourites", Snackbar.LENGTH_LONG).setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
                     DailyFeedModel.open();
-                    DailyFeedModel.insert(title, desc, imgUrl, url, date, checked);
+                    DailyFeedModel.deleteFav(listItems.get(position).getmHeading());
+                    Log.i("DEV", "Item deleted successfully");
                     DailyFeedModel.close();
-                    Snackbar.make(v,"Added to favourites",Snackbar.LENGTH_LONG).setAction("UNDO", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            DailyFeedModel.open();
-                            DailyFeedModel.deleteFav(listItems.get(position).getmHeading());
-                            Log.i("DEV", "Item deleted successfully");
-                            DailyFeedModel.close();
 
-                        }
-                    }).show();
-                } else {
+                }
+            }).show();
+        } else {
 //                    holder.heart.setChecked(false);
-                    checked = 0;
+            checked = 0;
         /*
                     DailyFeedModel.open();
                     DailyFeedModel.deleteFav(listItem.getmHeading());
@@ -132,23 +190,23 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
                     DailyFeedModel.close();
 
 */
+            DailyFeedModel.open();
+            DailyFeedModel.deleteFav(listItems.get(position).getmHeading());
+            Log.i("DEV", "Item deleted successfully");
+            DailyFeedModel.close();
+
+            Log.i("YAY", "TO DELETE: " + title);
+
+            Snackbar.make(v, "Removed from favourites", Snackbar.LENGTH_LONG).setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
                     DailyFeedModel.open();
-                    DailyFeedModel.deleteFav(listItems.get(position).getmHeading());
-                    Log.i("DEV", "Item deleted successfully");
+                    DailyFeedModel.insert(title, desc, imgUrl, url, date, checked);
+
                     DailyFeedModel.close();
-
-                    Log.i("YAY", "TO DELETE: " + title);
-
-                    Snackbar.make(v,"Removed from favourites",Snackbar.LENGTH_LONG).setAction("UNDO", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            DailyFeedModel.open();
-                            DailyFeedModel.insert(title, desc, imgUrl, url, date, checked);
-
-                            DailyFeedModel.close();
-                        }
-                    }).show();
                 }
+            }).show();
+        }
 
             /*DailyFeedModel.open();
             favData = DailyFeedModel.showFavourites();
@@ -165,38 +223,38 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
             }*/
 
 
-            }
-        });
-
     }
+    });
+
+}
 
     @Override
     public int getItemCount() {
         return listItems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView heading, description, publishedAt;
-        public Button explore;
-        public CheckBox heart;
-        public ImageView newsImage;
-        public LinearLayout linearLayout;
-        ImageButton share;
+    public TextView heading, description, publishedAt;
+    public Button explore;
+    public CheckBox heart;
+    ImageButton speaker;
+    public ImageView newsImage;
+    public LinearLayout linearLayout;
+    ImageButton share;
 
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        heading = itemView.findViewById(R.id.titleTextView);
+        description = itemView.findViewById(R.id.descTextView);
+        share = itemView.findViewById(R.id.sharebtn);
+        explore = itemView.findViewById(R.id.buttonExplore);
+        heart = itemView.findViewById(R.id.likeCheckBox);
+        newsImage = itemView.findViewById(R.id.image_news);
+        linearLayout = itemView.findViewById(R.id.layout);
+        publishedAt = itemView.findViewById(R.id.textViewPublishedAt);
+        speaker = itemView.findViewById(R.id.speakerButton);
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            heading = itemView.findViewById(R.id.titleTextView);
-            description = itemView.findViewById(R.id.descTextView);
-            share = itemView.findViewById(R.id.sharebtn);
-            explore = itemView.findViewById(R.id.buttonExplore);
-            heart = itemView.findViewById(R.id.likeCheckBox);
-            newsImage = itemView.findViewById(R.id.image_news);
-            linearLayout = itemView.findViewById(R.id.layout);
-            publishedAt = itemView.findViewById(R.id.textViewPublishedAt);
-
-
-        }
     }
+}
 }
